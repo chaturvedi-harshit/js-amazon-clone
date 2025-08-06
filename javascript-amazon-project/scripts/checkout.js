745ec96cad9ccf038b32ea3aad91a0245da6e97c
chaturvedi-mac-pro <harshitchaturvedi1996@gmail.com>
2025-08-06 10:23:52 +0530

    added dayjs to calculate delivery times

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -5,6 +5,13 @@ import {
 } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
+import { deliveryOptions } from "../data/deliveryOptions.js";
+
+const today = dayjs();
+const twoDaysAfter = today.add(2, "days").format("dddd, MMMM DD");
+// console.log(today.format("dddd, MMMM DD"));
+console.log(twoDaysAfter);
 
 let cartSummaryHTML = "";
 cart.forEach((cartItem) => {
@@ -60,7 +67,28 @@ cart.forEach((cartItem) => {
                 <div class="delivery-options-title">
                   Choose a delivery option:
                 </div>
-                <div class="delivery-option">
+                ${deliveryOptionsHTML(matchingProduct)}
+                
+                
+            </div>
+        </div>
+    </div>
+    `;
+});
+
+function deliveryOptionsHTML(matchingProduct) {
+  let html = "";
+  deliveryOptions.forEach((option) => {
+    const today = dayjs();
+    const deliveryDate = today.add(option.days, "days");
+    const dateString = deliveryDate.format("dddd, MMMM DD");
+    const priceString =
+      option.priceCents === 0
+        ? "FREE Shipping"
+        : `$${formatCurrency(option.priceCents)} - Shipping`;
+
+    html += `
+     <div class="delivery-option">
                   <input
                     type="radio"
                     checked
@@ -68,37 +96,14 @@ cart.forEach((cartItem) => {
                     name="delivery-option-${matchingProduct.id}"
                   />
                   <div>
-                    <div class="delivery-option-date">Tuesday, June 21</div>
-                    <div class="delivery-option-price">FREE Shipping</div>
+                    <div class="delivery-option-date">${dateString}</div>
+                    <div class="delivery-option-price">${priceString}</div>
                   </div>
-                </div>
-                <div class="delivery-option">
-                  <input
-                    type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}"
-                  />
-                  <div>
-                    <div class="delivery-option-date">Wednesday, June 15</div>
-                    <div class="delivery-option-price">$4.99 - Shipping</div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input
-                    type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}"
-                  />
-                  <div>
-                    <div class="delivery-option-date">Monday, June 13</div>
-                    <div class="delivery-option-price">$9.99 - Shipping</div>
-                  </div>
-                </div>
             </div>
-        </div>
-    </div>
     `;
-});
+  });
+  return html;
+}
 
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
 
